0d733a1f143ab00c4db9b827bafd4873d363ba11
Rickhunter-stack <olivierhurmic@gmail.com>
2026-01-20 13:00:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
+// Écouter les messages push
 self.addEventListener("push", (event) => {
   const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "icon.png", // Ajoute ton icon
-  });
+  
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/favicon.jpg",
+      badge: "/favicon.jpg",
+      vibrate: [400, 200, 400, 200, 400],
+      tag: 'alert-notification',
+      requireInteraction: true, // La notification reste jusqu'à interaction
+      data: {
+        url: '/' // URL à ouvrir au clic
+      }
+    })
+  );
+});
+
+// Gérer le clic sur la notification
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
 });
